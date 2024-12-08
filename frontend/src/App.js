12ff0737d53ffc4cc42cfb6cb67d5e30fa20f9e6
nferg04 <nferg04@iastate.edu>
2024-12-08 17:28:01 -0600

    Created a view all page for dishes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./NavBar.js";
+import Dishes from "./Dishes.js";
 
 
 
 function App() {
+
+    const [dishes, setDishes] = useState([]);
+
     return(
         <Router>
             <div className="d-flex">
@@ -14,6 +18,10 @@ function App() {
                     <h1 className="text-center">Welcome To Food Finder</h1>
                     <Routes>
                         <Route path="/" element={<div>To use Food Finder you can start on our Find Dishes page. Here you will be able to all of the dishes that users have had before. If there is a new dish you find that is not on this page you can add it in the Add Dishes page. You can add a dish to your saved dishes to your user by clicking the save dish button in each card. The saved dishes page will allow users to view any dish that they have saved and remove them if they want.</div>} />
+                        <Route path="/dishes" element = {<Dishes
+                            dishes = {dishes}
+                            setDishes = {setDishes}
+                        />}/>
                     </Routes>
                 </div>
             </div>
