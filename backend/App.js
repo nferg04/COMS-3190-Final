4d8392aae3789ac7fc857d6037fe5469cf92bb28
nferg04 <nferg04@iastate.edu>
2024-12-08 15:27:28 -0600

    Added Dependencies and General Mongo Connection

--- backend/App.js
+++ backend/App.js
@@ -13,10 +13,18 @@ const { MongoClient } = require("mongodb");
 
 // MongoDB
 const url = "mongodb://127.0.0.1:27017";
-const dbName = "SECOMS3190";
+const dbName = "coms3190Final";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/"); // Save images in the 'uploads' folder
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
+    }
+});
 
 app.listen(port, () => {
     console.log("App listening at http://%s:%s", host, port);
