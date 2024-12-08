a233e51b57636f36c2a5ca89db672153d11d45d6
John-Buttell <jbuttelliv@gmail.com>
2024-12-08 17:31:02 -0600

    small fixes
    added info to info page

---
+++ frontend/src/information.js
@@ -0,0 +1,40 @@
+//in the requirments what needs to be in here
+import React from "react"
+
+
+    const MyView = () => {
+    return (
+
+        <div>
+        
+<div id="generalInfo">
+        <div class="genInfoChild">
+            <h2>Class Name:</h2>
+            <p>SE/ComS 3190 Construction of User Interfaces</p>
+        </div>
+        <div class="genInfoChild">
+            <h2>Date:</h2>
+            <p>12/08/2024</p>
+        </div>
+    </div>
+    <div id="studentInfo">
+        <div class="eachInfo">
+            <h3>Nate Ferguson</h3>
+            <p>Email: nferg04@iastate.edu</p>
+        </div>
+        <div class="eachInfo">
+            <h3>John Buttell</h3>
+            <p>Email: jbuttell@iastate.edu</p>
+        </div>
+
+    </div>
+
+    <footer>
+        <p>&copy 2024 Nate Ferguson and John Buttell</p>
+    </footer>
+
+        </div>
+    );
+};
+
+export default MyView;
